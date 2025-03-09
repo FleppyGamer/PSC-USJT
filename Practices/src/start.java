bcfe367440378947c309c287b58e84d104f9dde1
Fleppy_Gamer <fleppyg@gmail.com>
2025-03-09 16:47:11 -0300

    start.java added

--- Practices/src/start.java
+++ Practices/src/start.java
@@ -2,6 +2,6 @@ package Practices.src;
 
 public class start {
     public static void main (String[] args) {
-        System.out.println("Hello World :3c");
+        System.out.println("Hello World :3c.");
     }
 }
